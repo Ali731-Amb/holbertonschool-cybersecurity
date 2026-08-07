d49f63ae230d8bc44797146add7143828824ca97
Ali731-Amb <alison.amblard.pro@gmail.com>
2026-08-07 11:54:27 +0200

    task08

--- linux_security/1x00_linux_fundamentals/8-log_policy.sh
+++ linux_security/1x00_linux_fundamentals/8-log_policy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mkdir -p "$1" && chown root:$2 "$1" && chmod 2770 $1
+mkdir -p "$1" && chown root:$2 "$1" && chmod 2750 $1
 cat << 'EOF' > /etc/logrotate.d/app
 /var/log/app/*.log {
     create 0640 root www-data
