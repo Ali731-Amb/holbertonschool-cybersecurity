2d0c01db266365c39e6b4fbcb06a4f43da158d7a
Ali731-Amb <alison.amblard.pro@gmail.com>
2026-08-07 18:42:51 +0200

    task5

--- linux_security/1x01_shell_ops/5-cleanup.sh
+++ linux_security/1x01_shell_ops/5-cleanup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while read -r username; do
 	if id "$username" >/dev/null 2>&1;  then
-    	passwd -l "$username" ; echo "User $username locked"
+    	user -L "$username" && echo "User $username locked"
 	else
     	echo "User $username not found"
 	fi
