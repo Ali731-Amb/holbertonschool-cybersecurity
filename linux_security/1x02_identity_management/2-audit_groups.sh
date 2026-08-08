b50408d96729d06e816eba6a03b2af2a2786e83b
Ali731-Amb <alison.amblard.pro@gmail.com>
2026-08-08 14:49:46 +0200

    TasK02

--- linux_security/1x02_identity_management/2-audit_groups.sh
+++ linux_security/1x02_identity_management/2-audit_groups.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for user in $(awk -F: '$3 >= 1000 {print $1}' "$1"); do
+awk -F: '$3 >= 1000 {print $1}' "$1" | while read user; do
 	for grp in disk docker shadow; do
 		if id -nG "$user" | grep -qw "$grp"; then
 			echo "$user:$grp"
