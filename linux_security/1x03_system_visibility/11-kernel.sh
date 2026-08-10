2ac219aed1819a33afdf16050c1b7a6783fc86c0
Ali731-Amb <alison.amblard.pro@gmail.com>
2026-08-10 17:02:43 +0200

    Task11

--- linux_security/1x03_system_visibility/11-kernel.sh
+++ linux_security/1x03_system_visibility/11-kernel.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-[ -f /var/log/kern.log ] && grep "segfault" /var/log/kern.log
-[ -f /var/log/messages ] && grep "segfault" /var/log/messages
+[ -f /var/log/kern.log ] && grep "segfault" /var/log/kern.log; [ -f /var/log/messages ] && grep "segfault" /var/log/messages
