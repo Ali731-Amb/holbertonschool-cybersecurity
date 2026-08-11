148908c3280de526a22c5e8508cf44d1e778fafe
Ali731-Amb <alison.amblard.pro@gmail.com>
2026-08-11 12:15:15 +0200

    Task00

--- linux_security/1x04_security_automation/sentinel.sh
+++ linux_security/1x04_security_automation/sentinel.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-[ -f sentinel.conf ] && . sentinel.conf && [ -n "$SERVICES" ] && [ -n "$FILES_TO_WATCH"] || { echo "Error: config file is missing" >&2 ; exit 1 ;  }
+[ -f sentinel.conf ] && . sentinel.conf && [ -n "$SERVICES" ] && [ -n "$FILES_TO_WATCH" ] || { echo "Error: config file is missing" >&2 ; exit 1 ;  }
